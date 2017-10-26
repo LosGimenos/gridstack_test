1b38af917bf4c46665fb17697f8a29fa157a5a71
Lorenzo (Larry) Hernandez <larryhernanjr@gmail.com>
2017-10-26 11:49:51 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ const config = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
-          presets: ['env', 'react'],
+          presets: ['es2015', 'react'],
         }
       },
       {
